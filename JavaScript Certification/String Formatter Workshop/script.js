e3e4e0ad37be52459a95517a6420373eb94c0469
paulo-rangel <paulorangel11n@gmail.com>
2026-01-19 09:50:08 -0300

    adds  String Formatter Workshop's folder and its progress up to step 6

---
+++ JavaScript Certification/String Formatter Workshop/script.js
@@ -0,0 +1,9 @@
+const userInput = "   Hello World!   ";
+console.log("Original input:");
+console.log(userInput);
+
+const cleanedInput = userInput.trim();
+console.log("Result of trimming whitespace from both ends:");
+console.log(cleanedInput);
+
+const trimmedStart = userInput.trimStart();
