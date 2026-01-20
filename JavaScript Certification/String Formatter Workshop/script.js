f611b9b4324ba083ad79d638f2fd66f3a05c3dd0
paulo-rangel <paulorangel11n@gmail.com>
2026-01-20 10:56:21 -0300

    Finishes String Formatter Workshop

--- JavaScript Certification/String Formatter Workshop/script.js
+++ JavaScript Certification/String Formatter Workshop/script.js
@@ -1,4 +1,4 @@
-const userInput = "   Hello World!   ";
+onst userInput = "   Hello World!   ";
 console.log("Original input:");
 console.log(userInput);
 
@@ -7,3 +7,22 @@ console.log("Result of trimming whitespace from both ends:");
 console.log(cleanedInput);
 
 const trimmedStart = userInput.trimStart();
+console.log("After using the trimStart() method, leading spaces removed:");
+console.log(trimmedStart);
+
+const trimmedEnd = userInput.trimEnd();
+console.log("After using the trimEnd() method, trailing spaces removed:");
+console.log(trimmedEnd);
+
+const upperCaseInput = cleanedInput.toUpperCase();
+console.log("Result of using the toUpperCase() method:");
+console.log(upperCaseInput);
+
+const lowerCaseInput = cleanedInput.toLowerCase();
+console.log("Result of using the toLowerCase() method:");
+console.log(lowerCaseInput);
+
+const lowercaseWord = "camelcase";
+const camelCasedVersion = lowercaseWord.slice(0, 5) + lowercaseWord[5].toUpperCase() + lowercaseWord.slice(-3)
+console.log("Camel cased version:");
+console.log(camelCasedVersion);
